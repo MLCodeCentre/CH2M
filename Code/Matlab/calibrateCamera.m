668d77489e0425089209dd382069540ecf996f4d
Tom <Tom@gmail.com>
2018-07-25 09:32:30 +0100

    committing after a long time, i have deleted old code after spending a while developing a new model

--- Code/Matlab/calibrateCamera.m
+++ Code/Matlab/calibrateCamera.m
@@ -2,18 +2,19 @@ function calibrateCamera(road,year)
 %% loading data
 close all
 params = config();
-% img_file = fullfile(dataDir(),road,year,'Images','2_2367_1174.jpg');
-% I = imread(img_file);
-% imshow(I);
-% figure;
+img_file = fullfile(dataDir(),road,year,'Images','2_2367_1174.jpg');
+I = imread(img_file);
+imshow(I);
+figure;
 
 calibrationData = readtable(fullfile(dataDir(),road,year,...
                             'calibration_data.csv'));
-num_data_points = size(calibrationData,1);
+%num_data_points = size(calibrationData,1);
+num_data_points = 2;
 
 %% 
-alpha = 10;
-gamma = 1.15;
+alpha = 0;
+gamma = 0;
 
 plotCamera('Location',[0,0,0],'Orientation',rotx(90),'Size',0.5)
 hold on
@@ -28,7 +29,7 @@ for row_ind = 1:num_data_points
    Camera = [row.photo_x, row.photo_y, row.photo_z];
    Pw = [row.x, row.y, row.z];
    Pc = rotz(row.Yaw + gamma)*rotx(alpha)*(Pw-Camera)';
-   Pc = [Pc(1) - params.r1, Pc(2) - params.r2, Pc(3) - params.r3];
+   Pc = [Pc(1) - params.r1, Pc(2) - params.r2, Pc(3) - params.r3]
    plot3(Pc(1),Pc(2),Pc(3),'ro')
    text(Pc(1),Pc(2),Pc(3),num2str(row_ind))
        
--- Code/Matlab/findRoad.m
+++ Code/Matlab/findRoad.m
@@ -7,27 +7,64 @@ I = imread(img_file);
 imshow(I);
 hold on
 
-dL1Lambda = 0.9;
-dL2Lambda = 1.4;
-
 params = config();
 
-alpha = 5;
-gamma = 0.3;
+% define table co-ordinates relative to the camera
+X = -2.3:0.1:1.09;
+X = X - params.r1;
 
-X = -2.22:0.5:1.65;
-Y = 16.4:55.7;
-Z = -2.5;
+Y = params.r2:26;
+Y = Y - params.r2;
+z = -2.5;
 
+m = 2560; cx = 1280; % >
+n = 2048; cy = 1024; % ^
+A = 0.6835841968e-1, B = 0.9914589887e-1, G = 0.3863973476e-2, L1 = 1.334465402, L2 = 1.576175343
+A = 0.9724188492e-1, B = 0.8872637983e-1, G = 0.5931674209e-2, L1 = 1.434725712, L2 = 1.106265542
+A = .7026536670, B = .4433739134, G = -1.192219028, L1 = 0.2078083419e-2, L2 = 0.7980890658e-1
+
+U = [];
+V = [];
 for x = X
-   for y = Y
-       r_w = [x,y,Z]';
-       r_c = rotz(gamma)*rotx(alpha)*r_w;
-       r_c = r_c + [params.r1, params.r2, params.r3]';
-       [u,v,phi,psi] = getPixelsFromRatio(r_c(1),r_c(2),r_c(3),...
-                                                  dL1Lambda,dL2Lambda,...
-                                                  params.m,params.n,...
-                                                  params.cx,params.cy);
-       plot(u,v,'ro')
-   end
+    for y = Y
+        u = m*L1*(...
+                 (x*(cos(G)*cos(B)) + y*(-sin(G)*cos(A) + cos(G)*sin(B)*sin(A)) + z*(sin(G)*sin(A) + cos(G)*sin(B)*cos(A)))/...
+                 (x*(sin(G)*cos(B)) + y*(cos(B)*cos(A) + sin(G)*sin(B)*sin(A)) + z*(-cos(G)*sin(A) + sin(G)*sin(B)*cos(A)))...
+                 );
+
+        v = n*L2*(...
+                 (x*(-sin(B)) + y*(cos(B)*sin(A)) + z*(cos(B)*cos(A)))/...
+                 (x*(sin(G)*cos(B)) + y*(cos(B)*cos(A) + sin(G)*sin(B)*sin(A)) + z*(-cos(G)*sin(A) + sin(G)*sin(B)*cos(A)))...
+                 );
+        u = u + cx;
+        v = -v + cy;
+        U = [U,u];
+        V = [V,v];
+    end
+    plot(U(:),V(:),'r')
+    hold on
+    U = [];
+    V = [];
+end
+
+for y = Y
+    for x = X
+        u = m*L1*(...
+                 (x*(cos(G)*cos(B)) + y*(-sin(G)*cos(A) + cos(G)*sin(B)*sin(A)) + z*(sin(G)*sin(A) + cos(G)*sin(B)*cos(A)))/...
+                 (x*(sin(G)*cos(B)) + y*(cos(B)*cos(A) + sin(G)*sin(B)*sin(A)) + z*(-cos(G)*sin(A) + sin(G)*sin(B)*cos(A)))...
+                 );
+
+        v = n*L2*(...
+                 (x*(-sin(B)) + y*(cos(B)*sin(A)) + z*(cos(B)*cos(A)))/...
+                 (x*(sin(G)*cos(B)) + y*(cos(B)*cos(A) + sin(G)*sin(B)*sin(A)) + z*(-cos(G)*sin(A) + sin(G)*sin(B)*cos(A)))...
+                 );
+        u = u + cx;
+        v = -v + cy;
+        U = [U,u];
+        V = [V,v];
+    end
+    plot(U(:),V(:),'r')
+    hold on
+    U = [];
+    V = [];
 end
